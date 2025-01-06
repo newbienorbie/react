472f13970ca7b008fc654c3771a3790f6136eff2
Norberta Sandrine <151909800+newbienorbie@users.noreply.github.com>
2025-01-06 14:47:58 +0800

    removed

--- github-finder-all/github-finder/src/components/layout/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from "react";
-import Search from "../../users/Search";
-import Users from "../../users/Users";
-export const Home = () => (
-  <Fragment>
-    <Search />
-    <Users />
-  </Fragment>
-);
